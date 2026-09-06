ff311b5bb6726733804eb8361b2087b3535a035d
ayushnautiyal309-web <ayushnautiyal309@gmail.com>
2026-09-06 08:36:53 +0530

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,19 @@
+print("---------------------------")
+print("  Welcome in quiz 2026     ")
+print("--------------------------")
+
+x = input("what is your name ?")
+
+ 
+print("welcome",x)
+print("who  invented computer?")
+print("a.ayush nautiyal ")
+print("b.charls baibej")
+print("right brother")
+e = input("chose you option,(a,b,c):")
+if (e=='b'):
+    print("youre option is right" )
+elif (e=='a'):
+    print("your option is worng")
+else:
+    print("your optionis worng")
