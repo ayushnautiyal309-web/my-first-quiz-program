5332e262a254be03614b2f19ba7250ef9bbca3a5
ayushnautiyal309-web <ayushnautiyal309@gmail.com>
2026-09-06 12:23:15 +0530

    Add files via upload

--- quiz.py
+++ quiz.py
@@ -3,9 +3,10 @@ print("  Welcome in quiz 2026     ")
 print("--------------------------")
 
 x = input("what is your name ?")
-
+score=0
  
 print("welcome",x)
+
 print("who  invented computer?")
 print("a.ayush nautiyal ")
 print("b.charls baibej")
@@ -13,7 +14,21 @@ print("right brother")
 e = input("chose you option,(a,b,c):")
 if (e=='b'):
     print("youre option is right" )
+    score=score+1
 elif (e=='a'):
     print("your option is worng")
 else:
-    print("your optionis worng")
+    print("your option worng")
+
+print("what is the capital of india ")
+print("a.dehradun")
+print("b.dehli")
+print("kolkata")
+ans=input("chose your ootion:(a/b/c)" )
+if (ans=='b'):
+    print("your option is right")
+    score= score +1
+else:
+    print("your option is worng" )
+
+print("your score is ",score)
